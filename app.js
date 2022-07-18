552cbca54ce263728606dc9da977854f4880e9d9
sjaanackermann <60600578+sjaanackermann@users.noreply.github.com>
2022-07-18 09:58:45 +0200

    playround function added

--- app.js
+++ app.js
@@ -12,3 +12,38 @@ function computerPlay(){
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "rock") {
+        if(computerSelection === "scissors") {
+            playerScore++;
+            return "You Win! Rock beats Scissors";
+        } else if(computerSelection === "paper") {
+            computerScore++;
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "It's a Tie!"
+        }
+    }
+    if(playerSelection === "paper") {
+        if(computerSelection === "rock") {
+            playerScore++;
+            return "You Win! Paper beats Rock";
+        } else if(computerSelection === "scissors") {
+            computerScore++;
+            return "You Lose! Scissors beats Paper";
+        } else {
+            return "It's a Tie!"
+        }
+    }
+    if(playerSelection === "scissors") {
+        if(computerSelection === "paper") {
+            playerScore++;
+            return "You Win! Scissors beats Paper";
+        } else if(computerSelection === "rock") {
+            computerScore++;
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "It's a Tie!"
+        }
+    }
+}
